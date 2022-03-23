58fb889a38a5e5034edabce17ae1d92885a12bf7
camargo <camargo.bruno.dev@gmail.com>
2022-03-23 10:32:35 -0300

    Backend project created

---
+++ backend/src/main/java/com/angular/crud/CrudApplication.java
@@ -0,0 +1,13 @@
+package com.angular.crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudApplication.class, args);
+	}
+
+}
